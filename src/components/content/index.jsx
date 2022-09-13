cdc37b4fa96c701f9eba796f38bdf66635c832b3
Alikhan Gubayev <jonasofan.gubaev@gmail.com>
2022-09-13 16:32:39 +0600

    kbtu

--- src/components/content/index.jsx
+++ src/components/content/index.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import "../../assets/fonts/joystix.ttf";
 
 export const Main = () => {
   return (
     <>
       <div className="main">
-        <h1>Hello, Turing!</h1>
+        <div className="main-wrapper">
+          <h1>Hello, Turing!</h1>
+          <h2>Building Progressive Community</h2>
+        </div>
       </div>
     </>
   );
